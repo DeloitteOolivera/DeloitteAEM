2f512229a35f9e005a9f7914b760019f4295bf25
DeloitteOolivera <ooliveraramirez@deloitte.com>
2026-06-01 12:52:00 -0600

    Update aem.js
    Added preview

--- scripts/aem.js
+++ scripts/aem.js
@@ -675,3 +675,8 @@ export {
   waitForFirstImage,
   wrapTextNodes,
 };
+
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
