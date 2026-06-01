86d73d18933bd2330fd06d72efedf7f4000c5cd1
DeloitteOolivera <ooliveraramirez@deloitte.com>
2026-06-01 17:20:36 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -188,7 +188,8 @@ async function loadPage() {
 
 loadPage();
 
-(async function loadDa() {
+/*(async function loadDa() {
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
   import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
+*/
